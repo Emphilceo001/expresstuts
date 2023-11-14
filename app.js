8f7eb70c1591c6c9129061528ba0519506a445d2
emphilceo <mikeadeyemi13@outlook.com>
2023-11-14 06:20:57 -0800

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 5050;
+const port = process.env.PORT || 5050;
 const morgan = require("morgan");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const connect = require("./db/mongoDB");
 require("dotenv/config");
+const Tasks = require("./model/taskModel");
 
-const mongoDBUrl = process.env.DBURL;
 //custom middlewares
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 // app.set("view engine", "ejs");
 // app.use((req, res) => {
 //   console.log("new request made");
@@ -16,6 +18,46 @@ app.set("view engine", "ejs");
 //   console.log("method", req.method);
 //   next();
 // });
+
+//TESTING DATABASE AND MODEL
+app.get("/post-task", async (req, res) => {
+  const testData = new Tasks({
+    name: "mike",
+    title: "express tuts",
+    task: "we started using mongoDB",
+  });
+
+  try {
+    const newTasks = await testData.save();
+    res.status(201).send(newTasks);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//.find() method is a mongoose method for getting all the data from our database
+
+app.get("/get-post", async (req, res) => {
+  try {
+    const getTasks = await Tasks.find();
+    res.status(200).send(getTasks);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//findById()
+
+app.get("/single-task", async (req, res) => {
+  try {
+    const singleTask = await Tasks.findById("6552299199c55cede19c0e1d");
+    res.status(200).send(singleTask);
+  } catch (error) {
+    console.log(error);
+  }
+});
+// END OF DATABASE TEST
+
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
@@ -24,32 +66,71 @@ app.use(express.static("public"));
 //   res.send("Welcome Home");
 // });
 
-const tasks = [
-  {
-    name: "Emphil",
-    title: "emphil clothing",
-    task: "client deliveries this morning",
-  },
-  {
-    name: "Steve",
-    title: "steve clothing",
-    task: "client deliveries this afternoon",
-  },
-  {
-    name: "Enoo",
-    title: "enoo clothing",
-    task: "client deliveries this evening",
-  },
-];
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home || Page ", tasks });
+// const tasks = [
+//   {
+//     name: "Emphil",
+//     title: "emphil clothing",
+//     task: "client deliveries this morning",
+//   },
+//   {
+//     name: "Steve",
+//     title: "steve clothing",
+//     task: "client deliveries this afternoon",
+//   },
+//   {
+//     name: "Enoo",
+//     title: "enoo clothing",
+//     task: "client deliveries this evening",
+//   },
+// ];
+
+//api
+
+app.post("/api/v1/create", async (req, res) => {
+  console.log(req.body);
+
+  const newTask = new Tasks(req.body);
+
+  try {
+    await newTask.save();
+    res.status().redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+app.get("/api/v1/route/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const result = await Tasks.findById(id);
+    res
+      .status(200)
+      .render("singlepage", { title: "single || page", task: result });
+  } catch (error) {
+    console.log(error);
+  }
+});
+//page route
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await Tasks.find();
+    res.render("index", { title: "Home || page", tasks: result });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// app.get("/", (req, res) => {
+//   res.render("index", { title: "Home || Page ", tasks });
+// });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About || Page " });
 });
 
-app.get("/task", (req, res) => {
+app.get("/tasks", (req, res) => {
   res.render("tasks", { title: "Task || Page " });
 });
 
@@ -59,8 +140,12 @@ app.use((req, res) => {
 
 // db connection
 
-mongoose.connect(mongoDBUrl).then(() => console.log("connected successfully!"));
-
-app.listen(PORT, () => {
-  console.log(`server connected to http://localhost:${PORT}`);
+connect().then(() => {
+  try {
+    app.listen(port, () => {
+      console.log(`server connected to http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log("cannot connect to the server");
+  }
 });
